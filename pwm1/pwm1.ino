66ba1ffdb15ba264d08b313b00d3e43432f1041b
Aaron Nielsen <apn@apnielsen.com>
2018-01-29 06:19:12 -0500

    added first sketch to play with pwm programming

---
+++ pwm1/pwm1.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  // board is arduino uno or adafruit pro trinket both of which
+  // use ATmega328p
+  // both have the same sets of pins exposed for digital PWM:
+  // 3,5,6,10,11
+  pinMode(3, OUTPUT);
+  pinMode(11, OUTPUT);
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS22);
+  OCR2A = 180;
+  OCR2B = 50;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(3,
+}
