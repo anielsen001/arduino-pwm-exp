ec1cba549670513c913220ad84ff73bf9aada676
Aaron Nielsen <apn@apnielsen.com>
2018-01-30 06:00:17 -0500

    experimenting

--- pwm1/pwm1.ino
+++ pwm1/pwm1.ino
@@ -4,15 +4,17 @@ void setup() {
   // board is arduino uno or adafruit pro trinket both of which
   // use ATmega328p
   // both have the same sets of pins exposed for digital PWM:
-  // 6,5   - TCCR0B
-  // 9,10  - TCCR1B
-  // 11,3  - TCCR2B - timer/counter registers
+  // 6,5   - TCCR0A/B - Timer 0
+  // 9,10  - TCCR1A/B - Timer 1
+  // 11,3  - TCCR2A/B - Timer 2 timer/counter registers
   // TCCRxA - 
   pinMode(3, OUTPUT);
   pinMode(11, OUTPUT);
-  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(CS22);
-  OCR2A = 180;
+  //TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  //TCCR2B = TCCR2B & B11111000 | B00000100; 
+  TCCR2B = TCCR2B & B11111000 | B00000011;
+  //TCCR2B = _BV(CS22);
+  //OCR2A = 180;
   OCR2B = 50;
 }
 
