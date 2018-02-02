d7817abe0d358e0a116610029e9c4d20b4bfe907
Aaron Nielsen <apn@apnielsen.com>
2018-02-02 05:54:01 -0500

    added switch to control motor on/off

--- motor1/motor1.ino
+++ motor1/motor1.ino
@@ -1,3 +1,11 @@
+
+const int buttonPin = 2;
+const int motorPin = 3; 
+
+// button stuff loosely adapted from 
+// https://www.arduino.cc/en/Tutorial/Button
+int buttonState = 0;  // variable for push button status
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -8,7 +16,7 @@ void setup() {
   // 9,10  - TCCR1A/B - Timer 1
   // 11,3  - TCCR2A/B - Timer 2 timer/counter registers
   // TCCRxA - 
-  pinMode(3, OUTPUT);
+  pinMode(motorPin, OUTPUT);
   //pinMode(11, OUTPUT);
   //TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   // TCCR2B = TCCR2B & B11111000 | B00000001; // no prescaler - too fast for motor
@@ -18,21 +26,27 @@ void setup() {
   //TCCR2B = _BV(CS22);
   //OCR2A = 180;
   OCR2B = 50;
+
+  // configure switch on pin 2 to use for input
+  pinMode(buttonPin,INPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // for first setup, configure the button to turn the motor on and off
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+  
   // empirically, driving the motor I have
-  //for (int ii = 256; ii>=0; ii -= 32) {
-  //  delay(1000);
-   // analogWrite(3,ii); 
-   //analogWrite(3,128+64); 
-   analogWrite(3,128); 
+    //analogWrite(3,128+64); 
+   analogWrite(motorPin,128); 
    //delay(1000);
-   //TCCR2B = TCCR2B & B11111000 | B00000100; // clk/64
-   //analogWrite(3,128); 
-   //delay(1000);
-   //TCCR2B = TCCR2B & B11111000 | B00000011; // clk/32
-  //}
+
+  } else {
+    // turn motor off
+    analogWrite(motorPin,0);
+  }
+
   
 }
